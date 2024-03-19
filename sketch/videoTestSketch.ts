aa435b1d6581af6483b6e2090b13d82a5f82e815
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-03-19 10:41:27 -0500

    made parcel template

--- sketch/videoTestSketch.ts
+++ sketch/videoTestSketch.ts
@@ -1,59 +1,35 @@
 import { VideoPixelReader } from "./VideoUtils";
 
-console.log("my code is running");
-
-let video1: VideoPixelReader;
-let setupDone = false;
-
-function benchmark(reader: VideoPixelReader) {
-    let seeks = 0;
-    let avgSeekTime = 0;
-    // Get pixel at coordinates (128, 128) and time 5 seconds into the video
-    for (let i = 0; i < 4; i++) {
-        for (let time = 0; time < 4; time += 0.1) {
-            seeks += 1;
-            let t0 = performance.now();
-            const c = reader.getPixel(0.5, time);
-            //console.log(`Pixel at (128, 128) at ${time} seconds: R=${c.r}, G=${c.g}, B=${c.b}`);
-            avgSeekTime += performance.now() - t0;
-        }
-    }
-    console.log(`avg seek time: ${avgSeekTime / seeks}`);
-}
+let t  = 0;
 
 window["setup"] = async function setup() {
     console.log("Setup Running");
-    createCanvas(100, 100);
-    video1 = new VideoPixelReader("Melody.webm", 24, 256);
-    //video2 = new VideoPixelReader("./assets/MelodyEncoded - Copy.webm", 24, 256);
-    let t0 = performance.now();
-    await Promise.all([video1.populateData()]);
-
-    console.log(`Time to decode: ${performance.now() - t0}`);
-    createCanvas(720, 400);
-    stroke(255); // Set line drawing color to white
-    frameRate(60);
-    setupDone = true;
+    createCanvas(windowWidth, windowHeight);
 };
 
-let time = 0;
+function loadingAnimation(progress: number){
+    background(255, 10);
+    push()
+    rectMode(CENTER);
+    translate(width / 2, height / 2);
+    rotate(progress * PI);
+    square(0,0, width / 4);
+    stroke(0,0,0);
+    square(0,0, width / 4 * progress);
+    pop()
+    textAlign(CENTER, CENTER)
+    textSize(70)
+    text("Loading", width / 2, height / 2);
+}
+
 window["draw"] = function draw() {
-    if (!setupDone) {
-        text("Loading", width / 2, height / 2);
+    t += deltaTime / 1000;
+    if (t < 5) {
+        loadingAnimation(t / 5);
         return;
     }
-    background(0);
-    stroke(0, 0);
-    time += deltaTime / 1000;
-    for (let i = 0; i < video1.resolution; i++) {
-        const pos = i / video1.resolution;
-        const col = video1.getPixel(pos, time % 4);
-        fill(col.r, col.g, col.b);
-        rect(
-            (i / video1.resolution) * width,
-            height,
-            2,
-            -(col.r + col.g + col.b)
-        );
-    }
+    background(255,0,255);
+    textAlign(CENTER, CENTER)
+    textSize(70)
+    text("Finished Loading", width / 2, height / 2);
 };
